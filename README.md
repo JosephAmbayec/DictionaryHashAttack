51e498f7510a4d786f8a86ded2460c55402206e3
JosephAmbayec <44503159+JosephAmbayec@users.noreply.github.com>
2022-10-17 17:00:20 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Python Script that allows you to find a password given it's hash.
 1. You must have python 3.x and install the hashlib module
 2. You must know the hash you are cracking
 3. You must find your own dictionary of passwords (or use the one I provided from https://github.com/danielmiessler/SecLists/blob/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt)
+
+
+### By Joseph Ambayec
--- dictionary.py
+++ dictionary.py
@@ -1,14 +1,28 @@
 import hashlib
+import datetime
 
-pass_hash = input('Enter the password hash: ')
-dictionary_path = input('Enter the filepath to your dictionary: ')
+# Input
+pass_hash = input('Enter the MD5 hash: ')
+dictionary_path = input('Enter the filepath to your dictionary (dict.txt): ')
+
+# Start Timer
+start = datetime.datetime.now()
+
+# Read Dictionary
 dict = open(dictionary_path, 'r')
-
 dict_lines = dict.readlines()
 
+# Main Loop
 for line in dict_lines:
     line = line.strip('\n')
     hash = hashlib.md5(line.encode()).hexdigest()
     if (pass_hash == hash):
         print("Found password: " + line)
         break
+
+# End Timer
+end = datetime.datetime.now()
+total = end - start
+
+print(f"It took {total.total_seconds()} seconds to run...")
+input("Press Enter to Close...")
